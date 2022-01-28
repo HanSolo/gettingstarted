7f494ef5929fb861b200aaf3d2c29018ddc53d5e
Gerrit Grunwald <han.solo@mac.com>
2022-01-28 09:39:24 +0100

    Added more info

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ## Getting started
+
+A little demo project which shows how to use libraries like
+
+[Charts](https://github.com/HanSolo/charts/tree/jdk17)
+
+[TilesFX](https://github.com/HanSolo/tilesfx/tree/jdk17)
+
+in combination with JDK 17
+
+Please take a look at the [build.gradle](https://github.com/HanSolo/gettingstarted/blob/main/build.gradle) file to see which imports are needed for
+those libraries.
+And in addition please also take a look at the [module-info.java](https://github.com/HanSolo/gettingstarted/blob/main/src/main/java/module-info.java) file which contains the
+required modules for each library;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,20 +6,25 @@ module eu.hansolo.fx.gettingstarted {
     requires transitive javafx.base;
     requires transitive javafx.graphics;
     requires transitive javafx.controls;
+    // needed for Charts and TilesFX
     requires transitive javafx.swing;
+    // needed for TilesFX
     requires transitive javafx.media;
     requires transitive javafx.web;
 
     // 3rd Party
+    // needed for Charts and TilesFX
     requires transitive eu.hansolo.jdktools;
     requires transitive eu.hansolo.toolbox;
     requires transitive eu.hansolo.toolboxfx;
-
     requires transitive eu.hansolo.fx.heatmap;
     requires transitive eu.hansolo.fx.countries;
+    // needed for Charts
     requires transitive eu.hansolo.fx.charts;
+    // needed for TilesFX
     requires transitive eu.hansolo.tilesfx;
 
+    // Exports
     exports eu.hansolo.fx.gettingstarted.charts;
     exports eu.hansolo.fx.gettingstarted.tilesfx;
 }
